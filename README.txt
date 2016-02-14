bd5a0cbe7d38dda2873243c375f90c3f78fa231f
nord <nord.chetbi@txdlimited.co.uk>
2016-02-14 13:08:52 +0000

    initial

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/nord-txd/pen/LGqJJY.
+
+ Wanted to illustrate my car to feature on my latest portfolio site, just having fun with animation.
---
+++ js/index.js
@@ -0,0 +1,59 @@
+console.clear()
+
+var obj = $('#car'),
+  moon = $('#moon'),
+  w1 = $('#wheelOne'),
+  w2 = $('#wheelTwo'),
+  c1 = $('#cloudOne'),
+  c2 = $('#cloudTwo'),
+  c3 = $('#cloudThree'),
+  doc = $(document);
+
+// Mouse movement
+function mouser() {
+  doc.mousemove(function(e) {
+    // move the car
+    var spin = e.pageX * 1,
+      carW = obj.outerWidth() / 2,
+      pos = e.pageX - carW;
+    TweenMax.set(w1, {
+      transformOrigin: 'center center'
+    })
+    TweenMax.set(w2, {
+      transformOrigin: 'center center'
+    })
+    TweenMax.to(obj, 0.1, {
+      x: pos
+    })
+    TweenMax.to(w1, 0.1, {
+      rotation: spin
+    })
+    TweenMax.to(w2, 0.1, {
+      rotation: spin
+    });
+    // Move the Moon 
+    var vMid = doc.width() / 2;
+    var hMid = doc.height() / 2;
+    var x = -(e.pageX - vMid) / 15;
+    var c1x = -(e.pageX - vMid) / 7;
+    TweenMax.to(moon, 0.1, {
+      x: -x
+    })
+    // Move the clouds
+    TweenMax.to(c1, 0.1, {
+      x: -c1x
+    })
+    TweenMax.to(c2, 0.1, {
+      x: -c1x
+    })
+    TweenMax.to(c3, 0.1, {
+      x: -c1x
+    })
+    
+  });
+}
+
+mouser();
+$(document).click(function() {
+  $('#light').toggle();
+});
